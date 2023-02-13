5473732e55a82711ab56b6c0f057511317a10955
Marcus Botelho <marcus.botelho@solides.com.br>
2023-02-13 08:42:48 -0300

    Create Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,11 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://avatars.githubusercontent.com/u/7614138?v=4"
-            alt=""
-          />
+          <Avatar src="https://avatars.githubusercontent.com/u/7614138?v=4" />
           <div className={styles.authorInfo}>
             <strong>Marcus Botelho</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { PencilLine } from "phosphor-react";
 
 import styles from "./Sidebar.module.css";
@@ -12,11 +13,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://avatars.githubusercontent.com/u/7614138?v=4"
-          alt=""
-        />
+      <Avatar src="https://avatars.githubusercontent.com/u/7614138?v=4" />
         <strong>Marcus Botelho</strong>
         <span>Web Developer</span>
       </div>
