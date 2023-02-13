f9193c58dd6597aeda04e7d4e299d61157f22098
Marcus Botelho <marcus.botelho@solides.com.br>
2023-02-13 08:12:38 -0300

    Change post and finish comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,37 @@
-import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
-    <div className={styles.comment}></div>
-  )
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/7614138?v=4" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Marcus Botelho</strong>
+              <time
+                title="2 de fevereiro às 21:25"
+                dateTime="2023--02-02 21:25:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom dev, parabéns!!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
